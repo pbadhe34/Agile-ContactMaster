a69e54e2b8e2edad652cdde6972eb77eb4e86ba1
Prakash <prakash.badhe@agilesoft.in>
2015-04-28 12:41:52 +0530

    ContactMaster project created on 28th April

---
+++ ContactMaster/src/test/agile/FirstTest.java
@@ -0,0 +1,23 @@
+package test.agile;
+
+import static org.junit.Assert.*;
+//import junit.framework.Assert.*;
+
+import org.junit.Test;
+
+
+public class FirstTest {
+	
+	@Test
+	public void testRun()
+	{
+	 System.out.println("FirstTest.testRun()");
+	 boolean flag = true;
+	 assertEquals(true, flag);
+	  
+	 assertTrue(flag==false);
+	}
+	
+	
+
+}
