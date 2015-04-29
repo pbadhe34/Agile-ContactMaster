3d0cc8423d041e2e38a57f9e8119497741dac727
Prakash <prakash.badhe@agilesoft.in>
2015-04-29 16:17:40 +0530

    MyClassTest Class added by pundprashant

---
+++ ContactMaster/src/test/agile/MyTestClass.java
@@ -0,0 +1,16 @@
+package test.agile;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class MyTestClass {
+	
+	@Test
+	public void MyClassTest()
+	{
+	 System.out.println("MyClassTest.testRun()");		  
+	 assertTrue(true);
+	}
+
+}
